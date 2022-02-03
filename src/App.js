3ff86e45a86f9d612b944ed9878e4f42bb264f05
Esteban POHIN <esteban.pohin86@gmail.com>
2022-02-03 17:51:45 +0100

    First commit, add .env, modification app.js, create components files with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+
+import AdminPanel from './components/Admin/AdminPanel/AdminPanel';
+import AdminRoutes from './components/Admin/AdminRoutes/AdminRoutes';
+import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
+import GamesList from './components/GamesList/GamesList';
+import MoviesList from './components/MoviesList/MovieList';
+import AdminCreateGame from './components/Admin/AdminCreateGame/AdminCreateGame';
+import AdminCreateMovie from './components/Admin/AdminCreateMovie/AdminCreateMovie';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GamesList />} />
+        <Route path="/movies" element={<MoviesList />} />
+        <Route path="/admin" element={<AdminRoutes />} />
+          <Route index element={<AdminPanel />} />
+          <Route path="games/create" element={<AdminCreateGame />} />
+          <Route path="movies/create" element={<AdminCreateMovie />} />
+      </Routes>
     </div>
   );
 }
